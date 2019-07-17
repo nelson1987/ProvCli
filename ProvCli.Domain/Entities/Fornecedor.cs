b0ae331622ce9d20abe3fa5e20de3b0af4f86e8d
Nelson <nelson.ash@outlook.com>
2019-07-17 17:25:48 -0300

    Create Domain

---
+++ ProvCli.Domain/Entities/Fornecedor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProvCli.Domain.Entities
+{
+    public class Fornecedor
+    {
+        public int Id { get; private set; }
+        public string RazaoSocial { get; private set; }
+        public string Email { get; private set; }
+        public string Cnpj { get; set; }
+        /*
+         * CADASTRO DE FORNECEDORES Nº: __________
+
+DADOS DOS FORNECEDOR
+
+Denominação social: __________________________________________
+
+Endereço: ___________________________________________________
+
+Bairro: _____________________   Cidade: ________________________
+
+UF: ____   Telefones: _________________   CEP: __________________
+
+Endereço eletrônico/e-mail: _____________________________________
+
+CNPJ: _______________________   Inscrição: _____________________
+         */
+    }
+}
